d820f1e5c853869b4f0d898aeeedcf3e3b8ce544
alankurniadi12 <alankurniadi53@gmail.com>
2021-11-19 21:04:56 +0700

    add splash screen

---
+++ app/src/main/java/com/ihsan/binarchallengechapter7/landingpage/LandingPageActivity.kt
@@ -0,0 +1,12 @@
+package com.ihsan.binarchallengechapter7.landingpage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ihsan.binarchallengechapter7.R
+
+class LandingPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing_page)
+    }
+}
