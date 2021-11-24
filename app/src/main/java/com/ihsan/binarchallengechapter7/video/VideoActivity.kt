c6b1b675e970870818e60c5dac1f73df82479169
firzann <62801880+firzann@users.noreply.github.com>
2021-11-24 14:08:57 +0700

    update video tutorial game

---
+++ app/src/main/java/com/ihsan/binarchallengechapter7/video/VideoActivity.kt
@@ -0,0 +1,59 @@
+package com.ihsan.binarchallengechapter7.video
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.VideoView
+import com.ihsan.binarchallengechapter7.MainActivity
+import com.ihsan.binarchallengechapter7.R
+
+class VideoActivity : AppCompatActivity() {
+    private val videoView: VideoView by lazy {
+        findViewById(R.id.video_view)
+    }
+
+    private val btnPlay: ImageButton by lazy {
+        findViewById(R.id.btn_play)
+    }
+
+    private val btnPause: ImageButton by lazy {
+        findViewById(R.id.btn_pause)
+    }
+
+    private val btnStop: ImageButton by lazy {
+        findViewById(R.id.btn_stop)
+    }
+
+    private val btnBack: Button by lazy {
+        findViewById(R.id.btn_back)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video)
+
+        val pathVideo = "android.resource://$packageName/${R.raw.tutorial}"
+        videoView.setVideoURI(Uri.parse(pathVideo))
+
+        btnPlay.setOnClickListener {
+            videoView.start()
+        }
+
+        btnPause.setOnClickListener {
+            videoView.pause()
+        }
+
+        btnStop.setOnClickListener {
+            videoView.stopPlayback()
+            videoView.setVideoURI(Uri.parse(pathVideo))
+        }
+
+        btnBack.setOnClickListener {
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
